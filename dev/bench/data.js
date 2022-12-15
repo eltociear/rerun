2adddf7cd14edd0c613b8630d104b00d7bda0af3
github-action-benchmark <github@users.noreply.github.com>
2022-12-15 15:01:34 +0000

    add Rust Benchmark (cargo) benchmark result for ead06b9eaf2eb797600169a2c12d353202572fa2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1671111463549,
+  "lastUpdate": 1671116494522,
   "repoUrl": "https://github.com/rerun-io/rerun",
   "entries": {
     "Rust Benchmark": [
@@ -141,6 +141,84 @@ window.BENCHMARK_DATA = {
             "name": "obj_batch_points_sequential/query",
             "value": 8581,
             "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "obj_batch_points_sequential/Tuid::random",
+            "value": 37,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "emil@rerun.io",
+            "name": "Emil Ernerfeldt",
+            "username": "emilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ead06b9eaf2eb797600169a2c12d353202572fa2",
+          "message": "Fix tensor crash (#568)",
+          "timestamp": "2022-12-15T15:53:46+01:00",
+          "tree_id": "fdfb5d54e148428d96f539b20823f06b56d5ed16",
+          "url": "https://github.com/rerun-io/rerun/commit/ead06b9eaf2eb797600169a2c12d353202572fa2"
+        },
+        "date": 1671116493280,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datastore/batch/rects/insert",
+            "value": 1682012,
+            "range": "± 4361",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datastore/batch/rects/query",
+            "value": 1339,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "obj_mono_points/insert",
+            "value": 1021106689,
+            "range": "± 3091099",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "obj_mono_points/query",
+            "value": 363504,
+            "range": "± 1371",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "obj_batch_points/insert",
+            "value": 98002210,
+            "range": "± 427576",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "obj_batch_points/query",
+            "value": 11336,
+            "range": "± 23",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "obj_batch_points_sequential/insert",
+            "value": 23102875,
+            "range": "± 235486",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "obj_batch_points_sequential/query",
+            "value": 8611,
+            "range": "± 19",
             "unit": "ns/iter"
           },
           {
